650f76dfb44d63705aa15a5abd8c845570ef09f0
puroguramumirimo <mesugorira58@gmail.com>
2022-08-21 12:40:37 +0000

    change controler

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  #end_user_stateメソッドをログイン前に実行する
+  #ただし、createアクションは除く
+  before_action :end_user_state, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +32,21 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def end_user_state
+   #【処理内容１】
+   #入力されたemailからアカウントを1件取得する
+   @end_user = EndUser.find_by(email: params[:end_user][:email])
+   #アカウントを取得できなかった場合、このメソッドを終了する
+   return if !@end_user
+   #【処理内容２】
+   #取得したアカウントのパスワードと入力されたパスワードが一致している（処理結果ture）
+   #かつ取得したアカウントのis_deletedカラムの中身がtureだった時【処理内容３】を実行する
+    if @end_user.valid_password?(params[:customer][:password]) && @end_user.is_deleted
+   #【処理内容３】
+   #【処理内容２】の結果がfalseだった場合は、新規登録画面に遷移する
+     redirect_to new_end_user_registration_path
+    end
+  end
+  
 end
