d942d08972726f3e9b80f9192cc8638dfe7868cb
puroguramumirimo <mesugorira58@gmail.com>
2022-10-17 12:47:18 +0000

    不具合修正

--- app/controllers/public/passwords_controller.rb
+++ app/controllers/public/passwords_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Public::EndUser::PasswordsController < Devise::PasswordsController
+class Public::EndUsers::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
